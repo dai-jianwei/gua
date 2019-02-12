f701e5e69a37e2b979eab9a6b79f9ff537394fc4
daijianwei <daijianwei@m-chain.com>
2019-02-12 08:36:53 +0800

    确定项目

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,12 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>Description</h2>
+    <p>This is a Vue-based personal component library</p>
+    <h2>components</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li><a href="" target="_blank">select</a></li>
+      <li><a href="" target="_blank">icon</a></li>
     </ul>
   </div>
 </template>
@@ -24,7 +17,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your D component library'
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+// import Router from './route'
 import App from './App.vue'
 
 new Vue({
   el: '#app',
+  // Router,
   render: h => h(App)
+  // components: { App },
 })
